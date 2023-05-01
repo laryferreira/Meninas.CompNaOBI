e7448fc679445226ff34b3128dc56d35b8726de1
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-01 17:20:24 -0300

    Update IdadeMônica.py

--- OBI2019/IdadeMônica.py
+++ OBI2019/IdadeMônica.py
@@ -1,8 +1,12 @@
-M = int(input())
-A = int(input())
-B = int(input())
+''''Olá meninas, tudo bem?
+Nesse problema, temos três entradas e devemos descobrir a idade do outro filho de dona Mônica (que deverá ser o mais velho).
+''''
 
-C = M - (A + B) 
+M = int(input()) #idade de dona mônica
+A = int(input()) #idade de um filho (a)
+B = int(input()) #idade de um filho (b)
 
-vetor_idades = [A,B,C]
-print(max(vetor_idades))
+C = M - (A + B) #idade de dona mônica - soma da idade dos outros filhos
+
+idades = [A,B,C] #organizando em uma lista os valores
+print(max(idades)) #selecionando o maior valor (max) entre os filhos
