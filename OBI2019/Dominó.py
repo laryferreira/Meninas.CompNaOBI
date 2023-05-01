5662319b55ad871f7b72144ef7c1cdf5ad06a757
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-01 16:26:45 -0300

    Update Dominó.py

--- OBI2019/Dominó.py
+++ OBI2019/Dominó.py
@@ -1,1 +1,11 @@
+''''Olá meninas, tudo bem? 
 
+Percebam que nesse problema vocês deverão prestar atenção na expressão dada no enunciado: ((N+1)*(N+2))/2. (Ele já dá a fórmula)
+
+Queremos saber o número de peças de um jogo do tipo duplo-n, em que n deve ser a nossa entrada, perceberam?''''
+
+n = int(input()) # a entrada será um número inteiro
+duplo_n = ((n+1) * (n+2))// 2 #preste atenção nos parênteses
+print(duplo_n)
+
+'''''Execute o código acima e compare as entradas e as saídas!''''
