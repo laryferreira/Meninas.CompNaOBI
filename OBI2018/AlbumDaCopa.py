ebc475bfe32bd03e13ab4c01e0d78f222bc71d11
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-01 16:45:47 -0300

    Create AlbumDaCopa.py

---
+++ OBI2018/AlbumDaCopa.py
@@ -0,0 +1,11 @@
+''''Olá meninas, tudo bem?
+Percebam que o nosso papel nesse problema é contar quantas figurinhas faltam para completar o álbum. 
+Mas cuidado! Devemos considerar que há figurinhas repetidas.
+Vamos definir nossas variáveis:
+N = espaços e figurinhas totais
+M = número de figurinhas compradas
+x = cada figurinha repetida''''
+N = int(input())
+M = int(input())
+for x in range(M):
+  x = int(input())
