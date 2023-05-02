b9073ab9b61e41d58825f0a76ffddf00787aae72
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-01 21:09:47 -0300

    Create NotaEsquecida.py

---
+++ OBI2019/NotaEsquecida.py
@@ -0,0 +1,10 @@
+''''Oi meninas, tudo bem?
+Nesse problema temos a nota de uma das provas e a média, precisamos encontrar a outra nota. 
+Perceba que a média é obtida por (a+b)/2. Logo, fazer o caminho oposto é uma boa alternativa''''
+
+a = int(input())
+m = int(input())
+b = (m * 2) - a #operação inversa
+print(b)
+
+''''Teste alguns casos e compare suas respostas!''''
