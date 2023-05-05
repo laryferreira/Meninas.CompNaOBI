27e912f9d2d6f22524aab2488ceb41841f62d68d
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-05 12:20:01 -0300

    Create QuantaMandioca.py

---
+++ Revisão/QuantaMandioca.py
@@ -0,0 +1,14 @@
+porcaoCurupira = int(input())
+porcaoBoitata = int(input())
+porcaoBoto = int(input())
+porcaoMapinguari = int(input())
+porcaoIara = int(input()) #entrada
+
+curupira = porcaoCurupira*300
+boitata=porcaoBoitata*1500
+boto=porcaoBoto*600
+mapinguari=porcaoMapinguari*1000
+iara=porcaoIara*150
+chica=225
+
+print(curupira+boitata+boto+mapinguari+iara+chica)
