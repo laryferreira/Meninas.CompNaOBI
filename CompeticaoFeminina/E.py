3b22b3f4f086c131b392692e5aaaf507b4934606
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-27 10:30:43 -0300

    Create E.py

---
+++ CompeticaoFeminina/E.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+for i in range(n):
+  for j in range(m):
+    if (i % 2 == 0):
+      print("#", end=" ")
+    elif (i % 4 == 1):
+      if j == m - 1
+        print("#", end=" ")
+      else:
+        print(".", end=" ")
+    elif (i % 4 == 3):  
+        if j == 0:
+          print("#", end=" ")
+        else:
+          print(".", end=" ")
+   print(end = "\n")
+          
