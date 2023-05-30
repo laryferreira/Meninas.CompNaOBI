59260712513a8e6e857748cf3eda1233bb3baa5a
Ana Clara <abarbosaborges17@gmail.com>
2023-05-29 23:28:18 -0300

    Create Lingua-do-P.py

---
+++ OBI2014/Lingua-do-P.py
@@ -0,0 +1,8 @@
+frase = input().split()
+frase_decodificada = list()
+for palavra in frase:
+    palavra_decodificada = ''
+    for index in range(1, len(palavra), 2):
+        palavra_decodificada = palavra_decodificada + palavra[index]
+    frase_decodificada.append(palavra_decodificada)
+print(' '.join(frase_decodificada))
